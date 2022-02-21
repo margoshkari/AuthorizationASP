4429c75614c10c6bb0578bba5f01d2545cac96f9
margo <student@25-03>
2022-02-21 10:19:29 +0200

    upd

--- AuthorizationASP/Connection.cs
+++ AuthorizationASP/Connection.cs
@@ -10,6 +10,23 @@ namespace AuthorizationASP
             connection = new SqlConnection("Data Source=server1;Initial Catalog=UsersData;User Id=student;");
             connection.Open();
         }
+        /*CREATE DATABASE [UsersData];
+USE UsersData;
 
+CREATE TABLE [UserCategory]
+(
+	[ID] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE [User]
+(
+	[ID] INT PRIMARY KEY IDENTITY,
+	[Login] VARCHAR(30) UNIQUE NOT NULL,
+	[Birthday] DATE NOT NULL,
+	[PassHash] VARCHAR(120) NOT NULL,
+	[IdCategory] INT NOT NULL,
+	FOREIGN KEY ([IdCategory]) REFERENCES [UserCategory]([ID])
+);*/
     }
 }
